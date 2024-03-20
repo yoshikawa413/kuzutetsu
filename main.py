1cf72d5b6a2c08f60a3ecfff4e03b53484f185af
yoshikawa413 <yoshikawa413@gmail.com>
2024-03-20 22:41:36 +0900

    test

--- main.py
+++ main.py
@@ -4,7 +4,23 @@ import time
 from botocore.client import Config
 
 
-def lambda_handler(event, context):
+def get_query_results(client, query_execution_id):
+    for i in range(10):
+        print(f"{i=}, {query_execution_id=}")
+        query_details = client.get_query_execution(QueryExecutionId=query_execution_id)
+        state = query_details["QueryExecution"]["Status"]["State"]
+        if state == "SUCCEEDED":
+            response_query_result = client.get_query_results(
+                QueryExecutionId=query_execution_id
+            )
+            print(response_query_result)
+            return response_query_result
+        else:
+            print(state)
+        time.sleep(1)
+
+
+def lambda_handler(_, __):
     """
 
     CREATE TABLE iceberg_table (id bigint, data string)
@@ -18,35 +34,26 @@ def lambda_handler(event, context):
     Returns:
         _type_: _description_
     """
-    s3_bucket: str = "s3://your_bucket/"
-    sql: str = "insert into iceberg_table (id) values (?);"
+    sql: str = "insert into iceberg_table (id) values (CAST(? AS varchar));"
 
     # client = boto3.client("athena", config=Config(parameter_validation=False))
     client = boto3.client("athena")
 
-    # ExecutionParameters は空文字を指定できないので、列指定から外す必要がある
-    # string 型の場合、最小文字数が 1
     response = client.start_query_execution(
         QueryString=sql,
-        QueryExecutionContext={"Database": "default"},
-        ResultConfiguration={"OutputLocation": s3_bucket},
+        QueryExecutionContext={
+            "Database": "glue-database",
+            "Catalog": "AwsDataCatalog",
+        },
+        WorkGroup="athena-test",
         ExecutionParameters=["1"],
     )
 
     query_execution_id = response["QueryExecutionId"]
 
-    for i in range(10):
-        print(f"{i=}, {query_execution_id=}")
-        time.sleep(1)
-        query_details = client.get_query_execution(QueryExecutionId=query_execution_id)
-        state = query_details["QueryExecution"]["Status"]["State"]
-        if state == "SUCCEEDED":
-            response_query_result = client.get_query_results(
-                QueryExecutionId=query_execution_id
-            )
-            print(response_query_result)
-            break
-        else:
-            print(state)
+    query_results = get_query_results(client, query_execution_id)
+
+    if not query_results:
+        client.stop_query_execution(QueryExecutionId=query_execution_id)
 
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
